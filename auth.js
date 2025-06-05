196b5799f2fdb9ff5cef60427f3f564e9bdba68f
Collins Matlala <121460963+Hhhhjuuuuuuuuu888uuuuuuuuuuuuuuuuuuu@users.noreply.github.com>
2025-06-06 00:58:37 +0200

    arrenged user auth properly

---
+++ auth.js
@@ -0,0 +1,13 @@
+function redirectToStudent() {
+    window.location.href = "studentLog_in.html"; 
+}
+
+function redirectToAdmin() {
+    window.location.href = "adminLog_in.html"; 
+}
+
+function resetPasswordBtn() {
+    window.location.href = "studentLog_in.html"; 
+}
+
+         
